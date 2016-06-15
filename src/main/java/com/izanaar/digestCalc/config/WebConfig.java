af1a7f6529af2ae9fadf5df5950af25bd8698a67
Izanaar <izanaar@gmail.com>
2016-06-15 11:48:43 +0300

    template resolver configured

--- src/main/java/com/izanaar/digestCalc/config/WebConfig.java
+++ src/main/java/com/izanaar/digestCalc/config/WebConfig.java
@@ -1,11 +1,43 @@
 package com.izanaar.digestCalc.config;
 
 import com.izanaar.digestCalc.web.IndexController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackageClasses = IndexController.class)
 public class WebConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(TemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        TemplateResolver templateResolver =
+                new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
 
 }
--- src/main/java/com/izanaar/digestCalc/web/IndexController.java
+++ src/main/java/com/izanaar/digestCalc/web/IndexController.java
@@ -4,16 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public void index( HttpServletResponse response) throws IOException {
-        response.getWriter().write("Hello.");
+    public String index() {
+        return "index";
     }
 
 }
